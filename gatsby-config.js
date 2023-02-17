cd5bc209615d8a2739d72daf6bbc0d0e92f21c61
VHromyk <dev@smart-ui.pro>
2023-02-17 11:44:05 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `dhamira-test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
